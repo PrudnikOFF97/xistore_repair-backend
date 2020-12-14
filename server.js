80d294660702ad222e5f879f3c8ed81853651684
Vladislav <PrudnikOFFVI@gmail.com>
2020-12-14 15:57:29 +0300

    fix: change status code to 202

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.post("/update/:id", urlencodedParser, async function (req, res) {
         repair[Object.keys(req.body)] = req.body[Object.keys(req.body)];
     }
     await repair.save();
-    res.sendStatus(200);
+    res.sendStatus(202);
 });
 
 async function postNote(dataObj){
